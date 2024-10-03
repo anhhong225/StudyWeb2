8884aac6e566a5cc83e19529a939ccc4f04ed582
Hong <heoshan@gmail.com>
2024-10-03 15:23:16 +0700

    task2

---
+++ Week 4/main.js
@@ -0,0 +1,9 @@
+//Create instance Vue with element id 'app'
+//Give that somw data
+var app = new Vue({
+    el:'#app',
+    data: {
+        product: 'Socks',
+        image : './assets/images/socks_blue.jpg'
+    }
+})
